421f8c5ee1c4f1dcdc5efbb9cdf5e9d222dbda6b
seanshen20 <34725930+seanshen20@users.noreply.github.com>
2018-11-22 22:59:52 +1100

    stage 5 error handler

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -19,7 +19,7 @@ export class RegisterComponent implements OnInit {
     this.authService.register(this.model).subscribe(() => {
       console.log('registraion successful');
     }, error => {
-      console.log('error');
+      console.log(error);
     });
   }
 
